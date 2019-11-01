941930b67ff8f320a92fc3b9f115c8c075242633
Jørund Jenssen Berg <jjberg@Jrunds-iMac.local>
2019-11-01 07:11:55 +0100

    Adding nodeModules chapter plus core.js module

---
+++ nodeModules/core.js
@@ -0,0 +1,13 @@
+ const path = require("path");
+// const dirUpLoads = path.join(__dirname, "www", "files", "uploads");
+// console.log(dirUpLoads);
+
+const jj = require("util");
+// jj.log(path.basename(__filename));
+//jj.log(" The name of the current file");
+const v8 = require("v8");
+jj.log(v8.getHeapCodeStatistics());
+
+
+
+
